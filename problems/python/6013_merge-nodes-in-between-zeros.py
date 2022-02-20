4918a3a699b98b4da3a586392128e0cf6381ab79
oxape <wxdeve@126.com>
2022-02-20 23:20:06 +0800

    solving python 6013

---
+++ problems/python/6013_merge-nodes-in-between-zeros.py
@@ -0,0 +1,17 @@
+from re import I
+from typing import *
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return 
+
+if __name__ == '__main__':
+    nums = [0, 1, 0, 3, 0, 2, 2, 0]
+    for e in nums:
+
+        tmp = ListNode(e, None)
