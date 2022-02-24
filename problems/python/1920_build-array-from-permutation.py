d0bccb25221bb5e3fec5302b386f83229959ff70
donglu <donglu@starot.com>
2022-02-24 20:54:55 +0800

    reviewing problems 3

--- problems/python/1920_build-array-from-permutation.py
+++ problems/python/1920_build-array-from-permutation.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2022-02-18 09:55:07
+LastEditTime: 2022-02-22 19:11:24
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: /leetcode/problems/python/1920_build-array-from-permutation.py
+'''
 from typing import *
 
 class Solution:
@@ -6,6 +14,13 @@ class Solution:
         for i, e in enumerate(nums):
             ans[i] = nums[nums[i]]
         return ans
+    
+    def buildArrayV1(self, nums: List[int]) -> List[int]:
+        for i, e in enumerate(nums):
+            tmp = nums[i]
+            nums[i] = nums[nums[i]]
+            nums[nums[i]] = tmp
+        return nums
 
 if __name__ == '__main__':
     nums = [5,0,1,2,3,4]
--- problems/python/3_longest_substring_without_repeating_characters.py
+++ problems/python/3_longest_substring_without_repeating_characters.py
@@ -53,6 +53,28 @@ class Solution(object):
             max_length = nums
         return max_length
 
+    def lengthOfLongestSubstringR1(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 由于都是可见字符，这里使用array可以获得比dict更好的时间性能，有与hash算法，当所有字符都出现时，可能空间上也能更优
+        bucket = [-1]*128
+        nums = 0
+        max_length = 0
+        for index, c in enumerate(s):
+            if bucket[ord(c)] >= 0:
+                if nums > max_length:
+                    max_length = nums
+                nums = min(index-bucket[ord(c)], nums+1)
+                bucket[ord(c)] = index
+            else:
+                nums += 1
+                bucket[ord(c)] = index
+        if nums > max_length:
+            max_length = nums
+        return max_length
+
 
 if __name__ == '__main__':
     s = "abcabcbb"
@@ -75,3 +97,18 @@ if __name__ == '__main__':
     print(Solution().lengthOfLongestSubstring2(s))
     s = "abba"
     print(Solution().lengthOfLongestSubstring2(s))
+    s = "tmmzuxt"
+    print(Solution().lengthOfLongestSubstring2(s))
+    print('------')
+    s = "abcabcbb"
+    print(Solution().lengthOfLongestSubstringR1(s))
+    s = "bbbbb"
+    print(Solution().lengthOfLongestSubstringR1(s))
+    s = "pwwkew"
+    print(Solution().lengthOfLongestSubstringR1(s))
+    s = "dvdf"
+    print(Solution().lengthOfLongestSubstringR1(s))
+    s = "abba"
+    print(Solution().lengthOfLongestSubstringR1(s))
+    s = "tmmzuxt"
+    print(Solution().lengthOfLongestSubstringR1(s))
