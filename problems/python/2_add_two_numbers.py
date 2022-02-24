d01f07f2df5e6926b7e1bcd98fffd279929e321d
donglu <donglu@starot.com>
2022-02-24 20:01:18 +0800

    review problem 2

--- problems/python/2_add_two_numbers.py
+++ problems/python/2_add_two_numbers.py
@@ -1,38 +1,69 @@
 # Definition for singly-linked list.
+from typing import List
+
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-
-def addTwoNumbers(l1, l2):
-    """
-    :type l1: ListNode
-    :type l2: ListNode
-    :rtype: ListNode
-    """
-    val = l1.val + l2.val
-    node = ListNode(val % 10)
-    temp_node = node
-    next_val = val // 10
-    l1 = l1.next
-    l2 = l2.next
-    while not (l1 is None and l2 is None):
-        val = 0
-        if l1:
-            val += l1.val
-            l1 = l1.next
-        if l2:
-            val += l2.val
-            l2 = l2.next
-        val = val + next_val
-        temp_node.next = ListNode(val % 10, None)
-        temp_node = temp_node.next
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        val = l1.val + l2.val
+        node = ListNode(val % 10)
+        temp_node = node
         next_val = val // 10
-    if next_val != 0:
-        temp_node.next = ListNode(next_val, None)
-    return node
+        l1 = l1.next
+        l2 = l2.next
+        while not (l1 is None and l2 is None):
+            val = 0
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            val = val + next_val
+            temp_node.next = ListNode(val % 10, None)
+            temp_node = temp_node.next
+            next_val = val // 10
+        if next_val != 0:
+            temp_node.next = ListNode(next_val, None)
+        return node
+    
 
+    def addTwoNumbersV1(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        left_val = 0
+        head = node = None
+        while not (l1 is None and l2 is None):
+            val = 0
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            val = val + left_val
+            if node is None:
+                head = node = ListNode(val%10)
+            else:
+                node.next = ListNode(val%10)
+                node = node.next
+            left_val = val // 10
+        if left_val != 0:
+            node.next = ListNode(left_val, None)
+        return head
+    
 
 def construct_list_node(l):
     """
@@ -63,5 +94,5 @@ if __name__ == '__main__':
     case_l2 = construct_list_node([5, 6, 4])
     print_list_node(case_l1)
     print_list_node(case_l2)
-    result_node = addTwoNumbers(case_l1, case_l2)
+    result_node = Solution().addTwoNumbersV1(case_l1, case_l2)
     print_list_node(result_node)
