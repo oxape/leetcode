8aab598e97594f670c6a6e23983ec2e3e1a80553
oxape <oxape@MacbookPro.com>
2022-03-27 17:51:37 +0800

    add python 494

--- python/1920_build-array-from-permutation.py
+++ python/1920_build-array-from-permutation.py
@@ -1,11 +1,3 @@
-'''
-Author: your name
-Date: 2022-02-18 09:55:07
-LastEditTime: 2022-02-22 19:11:24
-LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /leetcode/problems/python/1920_build-array-from-permutation.py
-'''
 from typing import *
 
 class Solution:
---
+++ python/494_target_sum.py
@@ -0,0 +1,13 @@
+from typing import *
+
+class Solution:
+    def findNumOfSub(self, other: int, nums: List[int], target: int) -> int;
+        
+
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        if len(nums) < 1
+
+if __name__ == '__main__':
+    nums = [1,1,1,1,1]
+    target = 3
+    print(Solution().buildArray(nums))
