c5b2f4979beca7a6f119ebeeefbb313cf29720c3
oxape@ubuntu220401.com <oxape@ubuntu220401.com>
2023-05-17 17:34:46 +0800

    完成0015

--- c++/0015.cc
+++ c++/0015.cc
@@ -7,26 +7,44 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int m;
         vector<vector<int>> results;
-        // for (int i=0; i<nums.size(); i++) {
-        //     for (int j=i+1; j<nums.size(); j++) {
-        //         for (int k=j+1; k<nums.size(); k++) {
-        //             if (nums[i]+nums[j]+nums[k] == 0) {
-        //                 vector<int> result = {nums[i], nums[j], nums[k]};
-        //                 sort(result);
-        //                 for (m=0; m<results.size(); m++) {
-        //                     if (results[m] == result) {
-        //                         break;
-        //                     }
-        //                 }
-        //                 if (m == results.size()) {
-        //                     results.push_back(result);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+        int last;
+        if (nums.size() < 3) {
+            return results;
+        }
+        quick_sort(nums, 0, nums.size());
+        for (int i=0; i<nums.size()-2;) {
+            if (nums[i] > 0) {
+                break;
+            }
+            int left = i+1;
+            int right = nums.size()-1;
+            int sum = 0-nums[i];
+            while (left < right) {
+                if (nums[left] > sum) {
+                    break;
+                }
+                if (nums[left] + nums[right] < sum) {
+                    left++;
+                } else if (nums[left] + nums[right] > sum) {
+                    right--;
+                } else {
+                    results.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    while (nums[left] == nums[left-1] && left < right) {
+                        left++;
+                    }
+                    right--;
+                    while (nums[right] == nums[right+1] && left < right) {
+                        right--;
+                    }
+                }
+            }
+            i++;
+            while (nums[i] == nums[i-1] && i<nums.size()-2) {
+                i++;
+            }
+        }
         return results;
     }
 
@@ -36,7 +54,7 @@ public:
         nums[indexa] = tmp;
     }
 
-    int partition(vector<int> nums, int start, int end) {
+    int partition(vector<int> &nums, int start, int end) {
         int pivot = nums[start];
         int k = start;
         for (int i=start+1; i<end; i++) {
@@ -49,7 +67,7 @@ public:
         return k;
     }
 
-    void quick_sort(vector<int> nums, int start, int end) {
+    void quick_sort(vector<int> &nums, int start, int end) {
         if (start >= end) {
             return;
         }
@@ -62,10 +80,16 @@ public:
 int main() {
     Solution solution;
 
-    vector<int> nums = {-1,0,1,2,-1,-4};
-    solution.quick_sort(nums, 0, nums.size());
-    for (int i=0; i<nums.size(); i++) {
-        printf("%d\n", nums[i]);
+    vector<int> nums = {
+        // -1,0,1,2,-1,-4
+        -2, 0, 1, 1, 2
+    };
+    
+    // solution.quick_sort(nums, 0, nums.size());
+    vector<vector<int>> results = solution.threeSum(nums);
+    for (int i=0; i<results.size(); i++) {
+        printf("[%d %d %d]\n", results[i][0], results[i][1], results[i][2]);
     }
+    exit(0);
     return 0;
 }
