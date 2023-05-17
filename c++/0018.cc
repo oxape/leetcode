fd31461b8cc6e7f0dc6f78e9d3dcf23351f58927
oxape@ubuntu220401.com <oxape@ubuntu220401.com>
2023-05-17 19:19:20 +0800

    提交0018.cc

--- c++/0018.cc
+++ c++/0018.cc
@@ -1,31 +1,47 @@
 #include <string>
 #include <vector>
+#include <array>
 #include <cstdio>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> results;
+        if (nums.size() < 4) {
+            return results;
+        }
+        quick_sort(nums, 0, nums.size());
+        for (int i=0; i<nums.size()-3;) {
+
+            auto result = threeSum(vector<int>(nums.begin()+i+1, nums.end()), target-nums[i]);
+            if (result.size() > 0) {
+                for (int k=0; k<result.size(); k++) {
+                    results.push_back({nums[i], result[k][0], result[k][1], result[k][2]});
+                }
+            }
+            i++;
+            while (nums[i] == nums[i-1] && i<nums.size()-3) {
+                i++;
+            }
+        }
+        return results;
+    }
+
+    vector<vector<int>> threeSum(const vector<int> &nums, int target) {
         vector<vector<int>> results;
         if (nums.size() < 3) {
             return results;
         }
-        quick_sort(nums, 0, nums.size());
         for (int i=0; i<nums.size()-2;) {
-            if (nums[i] > 0) {
-                break;
-            }
+            
             int left = i+1;
             int right = nums.size()-1;
-            int sum = 0-nums[i];
             while (left < right) {
-                if (nums[left] > sum) {
-                    break;
-                }
-                if (nums[left] + nums[right] < sum) {
+                if (nums[i] + nums[left] < target - nums[right]) {
                     left++;
-                } else if (nums[left] + nums[right] > sum) {
+                } else if (nums[i] + nums[left] > target - nums[right]) {
                     right--;
                 } else {
                     results.push_back({nums[i], nums[left], nums[right]});
@@ -80,14 +96,16 @@ int main() {
     Solution solution;
 
     vector<int> nums = {
-        // -1,0,1,2,-1,-4
-        -2, 0, 1, 1, 2
+        // 1,0,-1,0,-2,2
+        // 2,2,2,2,2,
+        // 1,-2,-5,-4,-3,3,3,5
+        1,0,-1,0,-2,2
     };
     
     // solution.quick_sort(nums, 0, nums.size());
-    vector<vector<int>> results = solution.threeSum(nums);
+    vector<vector<int>> results = solution.fourSum(nums, 0);
     for (int i=0; i<results.size(); i++) {
-        printf("[%d %d %d]\n", results[i][0], results[i][1], results[i][2]);
+        printf("[%d %d %d %d]\n", results[i][0], results[i][1], results[i][2], results[i][3]);
     }
     exit(0);
     return 0;
